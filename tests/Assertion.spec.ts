c6cf4f9763bf9af3f27027a7d7616461d6cc1bca
Manickam <manick.badsha@gmail.com>
2026-07-05 10:36:00 -0400

    Initial repo

---
+++ tests/Assertion.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test('Assertion validation',async  ({ page }) => {
+
+    await page.goto('https://saucedemo.com')
+    await expect(page.locator("input#user-name")).toHaveCount(1)
+    await expect(page.locator("[data-test='password']")).toBeEnabled()
+
+    //await expect(page.locator("[data-test='login-button']")).toBeDisabled()
+    await expect.soft(page.locator("[data-test='login-button']")).toBeDisabled()
+
+
+    await expect(page.locator("[data-test='login-button']")).toBeVisible()
+    //await expect(page.locator("[data-test='login-button']")).toBeHidden()
+    await expect.soft(page.locator("[data-test='login-button']")).toBeHidden()
+
+
+    await expect(page.locator("[data-test='login-button']")).toHaveText('Login')
+
+    await expect(page.locator("[data-test='login-button']")).toHaveAttribute('class', 'submit-button btn_action')
+
+    await expect(page.locator("[data-test='login-button']")).toHaveId('login-button')
+
+    await expect(page.locator("[data-test='login-button']")).toHaveValue('Login')
+
+    //await expect(page.locator("[data-test='login-button']")).toHaveCSS('background-color', 'rgb(0, 125, 255)')
+
+    await expect(page).toHaveTitle('Swag Labs')
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+
+    //await expect(page, "Title not matching").not.toHaveTitle('Swag Labs')
+
+
+
+
+})
---
+++ tests/Locator.spec.ts
@@ -0,0 +1,62 @@
+import {test} from "@playwright/test"
+
+/*
+test('Practice locators',async  ({page}) => {
+
+    await page.goto('https://www.saucedemo.com')
+    await page.locator("//*[@name='user-name']").fill("standard_user");
+    await page.locator("input#password").fill("secret_sauce");
+
+    //await page.locator(".submit-button.btn_action").click()
+
+    await page.locator("input[value='Login']").click()
+
+    await page.locator("text=Sauce LABS Backpack").click()
+
+    await page.locator("id=add-to-cart").click()
+
+    await page.locator("data-test=remove").click()
+
+}) 
+
+test('Practice locators2',async  ({page}) => {
+
+    await page.goto('https://www.saucedemo.com')
+    await page.locator(".form_group", {has: page.locator("input#user-name")}).click()
+    await page.locator(".form_group", {has: page.locator("input#user-name")}).pressSequentially("standard_user")
+
+    await page.locator(".form_group", {hasNot: page.locator("input#user-name")}).click()
+    await page.locator(".form_group", {hasNot: page.locator("input#user-name")}).pressSequentially("secret_sauce")
+
+    await page.locator("input[value='Login']").click()
+    //await page.locator("//a", {hasText: "Sauce Labs Backpack"}).click()
+    await page.locator(".inventory_item_name", {hasNotText: /Sauce./}).click()
+
+}) */
+
+test('Practice of getby locators',async  ({ page }) => {
+
+    await page.goto('https://demo.nopcommerce.com/login')
+    await page.getByLabel("Email:", { exact: true }).fill("testcodeautomate@gmail.com")
+    await page.getByPlaceholder("Search store").fill("Apple MacBook Pro 13-inch")
+    await page.getByRole("button", { name: "Search" }).click()
+
+
+    console.log(await page.getByText("New Cust").textContent())
+    await page.getByAltText("nopCommerce demo store").click()
+    await page.getByTitle("Show products in category Electronics").first().click()
+
+    await page.getByRole("link", { name: "Computers" }).click()
+
+    await page.getByTestId("product-item-4").click()
+
+}) 
+
+test('Practice of getby locators2',async  ({ page }) => {
+
+    await page.goto('https://saucedemo.com')
+    await page.getByTestId("username").fill("standard_user")
+    await page.getByTestId("password").fill("secret_sauce")
+
+
+})
